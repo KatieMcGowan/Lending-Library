027a78bb85e57a040d3d13316ca463909659af25
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-06-24 12:32:26 -0700

    Adds route to get new book page

--- controllers/books.js
+++ controllers/books.js
@@ -3,8 +3,27 @@ const router = express.Router();
 const Book = require("../models/book");
 const Library = require("../models/library");
 
-router.get("/library/:id/book", (req, res) => {
-  res.render("books/new.ejs")
+router.get("/", (req, res) => {
+  Library.find({}, (err, foundLibrary) => {
+    res.render("books/new.ejs", {
+      library: foundLibrary
+    });
+  });
+});
+
+router.post("/", (req, res) => {
+  Book.create(req.body, (err, createdBook) => {
+    if (err) {
+      res.send(err);
+    } else {
+      Library.findById(req.body.libraryId, (error, foundLibrary) => {
+        foundLibrary.books.push(createdBook);
+        foundLibrary.save((err, savedLibrary) => {
+          res.redirect("/library/:id")
+        });
+      });
+    };
+  });
 });
 
 module.exports = router;
